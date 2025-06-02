3196d1e7bdbeeaafbce05b99d4f968b6e5584e1e
Daniel Zenke <danielzenke94@gmail.com>
2025-06-03 00:28:16 +0200

    apply fibonacci patch

--- config.def.h
+++ config.def.h
@@ -55,11 +55,14 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+ 	{ "[@]",      spiral },
+ 	{ "[\\]",      dwindle },
 };
 
 /* key definitions */
@@ -105,6 +108,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 
@@ -147,8 +152,8 @@ static const Key keys[] = {
 
     // close window, restart & quit dwm
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-    { MODKEY|ShiftMask,             XK_r,      quit,           {1} }, // restart
-	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} }, // quit
+    { MODKEY|ControlMask|ShiftMask, XK_r,      quit,           {1} }, // restart
+	{ MODKEY|ControlMask|ShiftMask, XK_e,      quit,           {0} }, // quit
 };
 
 /* button definitions */

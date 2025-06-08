dfa8820b6c4670fd0162701bda9bcf6b58a57f5f
Daniel Zenke <danielzenke94@gmail.com>
2025-06-08 19:06:55 +0200

    fix: decrease font size

--- config.def.h
+++ config.def.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 29;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "CodeNewRomanNerdFont:size=12" };
-static const char dmenufont[]       = "CodeNewRomanNerdFont:size=12";
+static const char *fonts[]          = { "CodeNewRomanNerdFont:size=11" };
+static const char dmenufont[]       = "CodeNewRomanNerdFont:size=11";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
